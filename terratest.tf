afc03d927d87496ea782edf968fb6827539011c4
sanjgp <58510580+sanjgp@users.noreply.github.com>
2026-08-14 12:33:52 +0530

    Add RDP ingress rule to terratest.tf

--- terratest.tf
+++ terratest.tf
@@ -9,6 +9,12 @@ resource "aws_security_group" "app" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  },
+  ingress {
+    from_port   = 3389
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
